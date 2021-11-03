eda52f7977727a253e6ab7b32d7019bcb3ff701a
Gianluca <detomma@amazon.com>
2021-11-03 12:20:11 +0100

    add function descriptions for pack & unpack

--- tools/structures.py
+++ tools/structures.py
@@ -1,14 +1,40 @@
-#!/usr/bin/env python
-
 import jax.numpy as jnp
 import numpy as np
 
 
 def pack(d: dict) -> jnp.array:
+    """
+    It packs dictionary into a 1-dimensional JAX array.
+
+    Parameters
+    ----------
+    d: dict
+        Dictionary to pack.
+
+    Returns
+    -------
+    a: jnp.array
+        1-dimensional JAX array corresponding to `d`.
+    """
     return jnp.concatenate([jnp.ravel(v) for v in d.values()])
 
 
 def unpack(a: jnp.array, d: dict) -> dict:
+    """
+    It unpacks a 1-dimensional JAX array into a dictionary with keys and value shapes like in `d`.
+
+    Parameters
+    ----------
+    a: jnp.array
+        Array to unpack.
+    d: dict
+        The unpacked version of `a` should have the same keys and value shapes as this dictionary.
+
+    Returns
+    -------
+    unpacked: dict
+        Dictionary corresponding to `d`-like unpacked version of `a`.
+    """
     keys, values = [], []
     for k, v in d.items():
         keys.append(k), values.append(v)
@@ -17,10 +43,3 @@ def unpack(a: jnp.array, d: dict) -> dict:
     cum_sizes = np.cumsum([0] + [v.size for v in values]).tolist()
     return {keys[i]: a[cum_sizes[i]:cum_sizes[i+1]].reshape(shapes[i]) for i in range(len(d))}
 
-
-if __name__ == '__main__':
-    d = {'a': jnp.array(np.arange(24).reshape(2,3,4)), 'b': jnp.array([]), 'c': jnp.array([5., 6.])}
-    a = jnp.array(np.random.normal(size=sum([v.size for v in d.values()])))
-
-    print('pack', pack(d))
-    print('unpack', a, unpack(a, d))
