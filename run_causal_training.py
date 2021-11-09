2601e9bd76b7a5bf4d57718e2a776d758aee2dcd
Gianluca <detomma@amazon.com>
2021-11-09 16:41:33 +0100

    generate data from linear model and infer it with nf model

--- run_causal_training.py
+++ run_causal_training.py
@@ -13,41 +13,47 @@ import time
 
 if __name__ == '__main__':
     dim = 2
-    n_obs = 1000
-    n_samples = 1000000
+    n_obs = 10000
+    n_samples = 100
 
-    from models.nf_confounder_model import define_model
-    model = define_model(dim=dim)
-    true_theta = {k: model['init_params'][k](i) for i, k in enumerate(model['init_params'])}
+    from models.linear_confounder_model import define_model
+    linear_model = define_model(dim=dim)
+    true_theta = {k: linear_model['init_params'][k]() for k in linear_model['init_params']}
 
-    cp = CausalProb(model=model)
-    u, v = cp.fill({k: u(n_obs, true_theta) for k, u in cp.draw_u.items()}, {}, true_theta, cp.draw_u.keys())
+    linear_cp = CausalProb(model=linear_model)
+    u, v = linear_cp.fill({k: u(n_obs, true_theta) for k, u in linear_cp.draw_u.items()}, {}, true_theta, linear_cp.draw_u.keys())
     x = v['X']
     o = {'V1': v['V1']}
     y = v['Y']
 
-    theta0 = {k: model['init_params'][k](10 + i) for i, k in enumerate(model['init_params'])}
-
-    oy = {**o, 'Y': y}
-
-    def loss(_theta: dict):
-        u_prior = {k: u(n_samples, _theta) for k, u in cp.draw_u.items()}
-
-        def _loss(xj, oyj):
-            u, v = cp.fill(u_prior, {**oyj, 'X': xj}, _theta, cp.draw_u.keys())
-
-            def __loss(i: int):
-                ui = {k: _u[i] if _u.ndim > 1 else _u for k, _u in u.items()}
-                vi = {k: _v[i] if _v.ndim > 1 else _v for k, _v in v.items()}
-                llkd = cp.llkd(ui, xj, oyj, _theta, vi)
-                return llkd
-            return -jnp.mean(jnp.vectorize(__loss)(range(n_samples)))
-
-        return vmap(_loss, (0, {k: 0 for k in oy}))(x, oy)
+    from models.nf_confounder_model import define_model
+    nf_model = define_model(dim=dim)
+    theta0 = {k: nf_model['init_params'][k](i) for i, k in enumerate(nf_model['init_params'])}
 
     start_time = time.time()
     print('Train model parameters...')
-    theta, losses = train(model, x, y, o, theta0, loss_type='neg-avg-log-evidence', step_size=1e-4, n_iter=10000)
+    theta, losses = train(nf_model, x, y, o, theta0, loss_type='neg-avg-log-evidence', step_size=1e-2, n_iter=1000)
     print('Training completed in {} seconds.'.format(np.round(time.time() - start_time, 2)))
     plt.plot(losses)
     plt.show()
+
+    nf_cp = CausalProb(model=nf_model)
+    _, est_v = nf_cp.fill({k: u(n_samples, theta) for k, u in nf_cp.draw_u.items()}, {}, theta, nf_cp.draw_u.keys())
+
+    plt.figure(figsize=(10, 6))
+    for i, rv, in enumerate(u):
+        plt.subplot(3, 2, 2 * i + 1)
+        plt.title(rv, fontsize=15)
+        plt.scatter(v[rv][:, 0], v[rv][:, 1], s=1, color='blue')
+        plt.legend(['true samples'], fontsize=12)
+        plt.xlim([-4, 4])
+        plt.ylim([-4, 4])
+        plt.subplot(3, 2, 2 * i + 2)
+        plt.title(rv, fontsize=15)
+        plt.scatter(est_v[rv][:, 0], est_v[rv][:, 1], s=1, color='red')
+        plt.legend(['estimated samples'], fontsize=12)
+        plt.xlim([-4, 4])
+        plt.ylim([-4, 4])
+    plt.tight_layout()
+    plt.show()
+
